aba2c10d90201be2bb7046f40d171826cb204b58
anushka3285 <anushkakakade8@gmail.com>
2025-02-23 10:52:00 +0530

    adding all files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="https://i.pinimg.com/564x/9f/d9/98/9fd9986f0e671473ba6b9f9b74454a66.jpg" class="img-fluid w-100 pt-1" alt="..."/ >
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2ujZnmY9uYTxyq67mvxehIL63HFv8sdO3EQ&s" height="400px" width="400px"class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Dal Bati</h5>
+        <p class="card-text">Dal bati is a traditional Indian dish of lentils, baked wheat balls, and powdered cereal..</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.rasabali.in/wp-content/uploads/2023/09/Puran-Poli.png" height="400px" width="400px" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">PuranPoli</h5>
+        <p class="card-text">This lentil stuffed sweet flatbread is a popular Maharashtrian recipe made during Ganesh Chaturthi, Diwali, Holi or any other festive occasion..</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://sunright.in/wp-content/uploads/2019/11/nov-blog1.png" height="400px" width="400px"class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Fish Fry</h5>
+        <p class="card-text">A fish fry is a social event containing battered or breaded fried fish.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://palatesdesire.com/wp-content/uploads/2022/09/Mysore-masala-dosa-recipe@palates-desire.jpg"  height="400px" width="400px"class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Dosa</h5>
+        <p class="card-text">A dosa is a thin, savoury crepe in Indian cuisine made from a fermented batter of ground black gram and rice.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+};
+
+export default Home
